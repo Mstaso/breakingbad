4e24136f18fc0a246eda8985009834a2990bb676
malcolm <mstaso1@gmail.com>
2021-04-27 12:45:44 -0400

    added pagination functionality

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import axios from 'axios'
 import Header from './components/ui/Header'
 import CharacterGrid from './components/characters/CharactarGrid'
 import Search from './components/ui/Search'
+import Pagination from './components/ui/Pagination'
 
 const App = () => {
-  const [items, setItems] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
-  const [query, setQuery] = useState('')
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(8);
+  const [query, setQuery] = useState('');
 
   useEffect(() => {
     const fetchItems = async () => {
@@ -24,11 +27,20 @@ const App = () => {
   }, [query])
 // query is now a dependancy and the useEffect fires off every time the query state is changed
 
+
+// get current items
+const indexOfLastItem = currentPage * itemsPerPage;
+const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+const currentItems = items.slice(indexOfFirstItem, indexOfLastItem);
+
+// Change Page
+const paginate = (pageNumber) => setCurrentPage(pageNumber)
   return (
     <div className="container">
    <Header />
    <Search getQuery={(q) => setQuery(q)}/>
-   <CharacterGrid isLoading={isLoading} items={items}/>
+   <CharacterGrid isLoading={isLoading} items={currentItems}/>
+   <Pagination itemsPerPage={itemsPerPage} totalItems={items.length} paginate={paginate}/>
     </div>
   );
 }
